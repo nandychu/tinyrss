d3c5fb8c1000c2d800b92fa18f2b9c3ab56ab657
nandychu <nandychu@gmail.com>
2024-02-12 10:21:00 +0100

    Added two base feed sources for demo purposes

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,23 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Text, StyleSheet, StatusBar, View } from "react-native";
 import { fetchFeed } from "../services/FetchService";
 import { RSSFeedSource } from "../models/RSSFeedSource";
 import { SafeAreaView } from "react-native-safe-area-context";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 export default function HomeScreen() {
+  const [initialLoad, setInitialLoad] = useState(false);
+
   useEffect(() => {
-    const xatakaAndroid: RSSFeedSource = {
-      name: "XatakaAndroid",
-      url: "https://www.xatakandroid.com/tag/feeds/rss2.xml",
-      firstFetch: false,
-      articles: [],
-    };
-    fetchFeed(xatakaAndroid);
+    getData();
   }, []);
+
+  const getData = async () => {
+    try {
+      const macStories = await AsyncStorage.getItem("macstories");
+      const xatakaAndroid = await AsyncStorage.getItem("xatakandroid");
+
+      if (!macStories) {
+        const macStoriesFeed: RSSFeedSource = {
+          name: "MacStories",
+          url: "mac",
+          firstFetch: false,
+          articles: [],
+        };
+        await AsyncStorage.setItem("macstories", JSON.stringify(macStoriesFeed));
+      }
+      if (!xatakaAndroid) {
+        const xatakaAndroid: RSSFeedSource = {
+          name: "XatakaAndroid",
+          url: "https://www.xatakandroid.com/tag/feeds/rss2.xml",
+          firstFetch: false,
+          articles: [],
+        };
+        await AsyncStorage.setItem("xatakandroid", JSON.stringify(xatakaAndroid));
+      }
+      setInitialLoad(true);
+    } catch (e) {
+      setInitialLoad(true);
+    }
+  };
 
   return (
     <SafeAreaView style={[styles.safeAreaContainter]}>
       <StatusBar barStyle="dark-content" />
-      <View style={[styles.listContainer]}></View>
+      <View style={[styles.listContainer]}>{initialLoad && <Text>Loaded</Text>}</View>
     </SafeAreaView>
   );
 }
